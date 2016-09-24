29213d1027ba25df0693c639aaa8fb658ee34c68
uehara1414 <b1015062@fun.ac.jp>
2016-09-24 17:44:08 +0900

    Update search

--- app/api/jalan/search.py
+++ app/api/jalan/search.py
@@ -37,13 +37,57 @@ def get_plans(wx, wy, range=1):
 
     return plans_to_json(plans)
 
+def _extract_from_plan(plan, obj, s):
+    try:
+        obj[s] = plan.find(s).text
+    except:
+        obj[s] = ""
+
 def plans_to_json(plans):
     objects = []
+    for plan in plans:
+         print(plan.prettify())
 
     for plan in plans:
         obj = dict()
-        obj["planname"] = plan.find("planname").text
-        obj["roomname"] = plan.find("roomname").text
+        _extract_from_plan(plan, obj, "planname")
+        _extract_from_plan(plan, obj, "plancd")
+        _extract_from_plan(plan, obj, "roomname")
+        _extract_from_plan(plan, obj, "roomcd")
+        _extract_from_plan(plan, obj, "plandetailurl")
+        _extract_from_plan(plan, obj, "plancommondetailurl")
+        _extract_from_plan(plan, obj, "plancheckin")
+        _extract_from_plan(plan, obj, "plancheckout")
+        _extract_from_plan(plan, obj, "splyperiodstrday")
+        _extract_from_plan(plan, obj, "splyperiodendday")
+        _extract_from_plan(plan, obj, "planpictureurl")
+        _extract_from_plan(plan, obj, "planpicturecaption")
+        _extract_from_plan(plan, obj, "meal")
+        _extract_from_plan(plan, obj, "ratetype")
+        _extract_from_plan(plan, obj, "samplerate")
+        _extract_from_plan(plan, obj, "servicechangerate")
+        _extract_from_plan(plan, obj, "hotelid")
+        _extract_from_plan(plan, obj, "hotelname")
+        _extract_from_plan(plan, obj, "postcode")
+        _extract_from_plan(plan, obj, "hoteladdress")
+        _extract_from_plan(plan, obj, "area")
+        _extract_from_plan(plan, obj, "region")
+        _extract_from_plan(plan, obj, "prefecture")
+        _extract_from_plan(plan, obj, "largearea")
+        _extract_from_plan(plan, obj, "smallarea")
+        _extract_from_plan(plan, obj, "hoteltype")
+        _extract_from_plan(plan, obj, "hoteldetailurl")
+        _extract_from_plan(plan, obj, "hotelcatchcopy")
+        _extract_from_plan(plan, obj, "hotelcaption")
+        _extract_from_plan(plan, obj, "pictureurl")
+        _extract_from_plan(plan, obj, "picturecaption")
+        _extract_from_plan(plan, obj, "x")
+        _extract_from_plan(plan, obj, "y")
+        _extract_from_plan(plan, obj, "hotelnamekana")
+        _extract_from_plan(plan, obj, "numberofratings")
+        _extract_from_plan(plan, obj, "rating")
+
+
         obj["facilities"] = []
         for facility in plan.find_all("facility"):
             obj["facilities"].append(facility.text)
